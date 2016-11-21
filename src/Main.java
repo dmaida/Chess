c4a460b7543d697cc9521d890a231a43f5c97b18
Daniel Esteban Maida <daniel.maida@wsu.edu>
2016-11-20 20:24:22 -0800

    Castling successfully works. No bugs found yet. Working on pawn transformation

--- src/Main.java
+++ src/Main.java
@@ -24,10 +24,38 @@ public class Main extends Application {
         Piece p = B.Tiles[cY][cX].getPiece();
         return p.getMoves(cX, cY);
     }
+    private static void castling(Board B, Piece p){
+        int dir = 1;
+        Piece rook;
+
+        if (p.name.contains("b")){
+            dir = -1;
+        }
+        if (p.name.contains("King") && ((nX == cX + (2 * dir)) | (nX == cX - (2 * dir)))){
+            if (nX == cX + (2 * dir)) {
+                rook = B.Tiles[nY][nX + (dir * 1)].getPiece();
+                B.Tiles[nY][nX + (dir * 1)].isOccupied = false;
+                B.Tiles[nY][nX + (dir * 1)].currentPiece = null;
+
+                B.Tiles[nY][nX - (dir * 1)].isOccupied = true;
+                B.Tiles[nY][nX - (dir * 1)].currentPiece = rook;
+            }
+            else {
+                rook = B.Tiles[nY][nX - (dir * 2)].getPiece();
+                B.Tiles[nY][nX - (dir * 2)].isOccupied = false;
+                B.Tiles[nY][nX - (dir * 2)].currentPiece = null;
+
+                B.Tiles[nY][nX + (dir * 1)].isOccupied = true;
+                B.Tiles[nY][nX + (dir * 1)].currentPiece = rook;
+            }
+        }
+    }
 
     public static void makeMove(Board B) {
 
         Piece p = B.Tiles[cY][cX].getPiece();
+
+        castling(B, p);
 
         if(p.isValidMove(cX,cY,nX,nY)){
             B.Tiles[cY][cX].isOccupied = false;
